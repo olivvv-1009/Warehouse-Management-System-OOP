9a5e945568722ad671c41555dacd490232aa483b
Thu <thudinh.31241026994@st.ueh.edu.vn>
2026-03-16 17:10:08 +0700

    inventory update

---
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/Models/Inventory/IStockManageable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Models.Inventory
+{
+    public interface IStockManageable
+    {
+        void AddStock(int amount);
+        void RemoveStock(int amount);
+    }
+}
---
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/Models/Inventory/Zone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace WMS.Models.Inventory
+{
+    public class Zone : StorageUnit
+    {
+        public List<Shelf> Shelves { get; set; }
+
+        public Zone()
+        {
+            Shelves = new List<Shelf>();
+        }
+
+        public void AddShelf(Shelf shelf)
+        {
+            Shelves.Add(shelf);
+        }
+
+        public List<InventoryItem> GetInventoryItems()
+        {
+            List<InventoryItem> items = new List<InventoryItem>();
+
+            foreach (var shelf in Shelves)
+            {
+                foreach (var batch in shelf.Batches)
+                {
+                    items.AddRange(batch.Items);
+                }
+            }
+
+            return items;
+        }
+    }
+}
