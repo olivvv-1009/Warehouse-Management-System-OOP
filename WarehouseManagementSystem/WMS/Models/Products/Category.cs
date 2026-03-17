f2b0765077362c86501beda6914dcd9cbaedb948
gnoohtwh <thongismyname1507@gmail.com>
2026-03-17 14:12:25 +0700

    Dependency

--- WarehouseManagementSystem/WMS/Models/Products/Category.cs
+++ WarehouseManagementSystem/WMS/Models/Products/Category.cs
@@ -2,4 +2,19 @@ public class Category
 {
     public string CategoryId { get; set; }
     public string CategoryName { get; set; }
+    public Category GetCategory(FoodProduct foodProduct)
+    {
+        this.CategoryName = "Food";
+        return this.CategoryName;
+    }
+    public Category GetCategory(ConsumableProduct consumableProduct)
+    {
+        this.CategoryName = "Consumable";
+        return this.CategoryName;
+    }
+    public Category GetCategory(ElectronicProduct eProduct)
+    {
+        this.CategoryName = "Electronic";
+        return this.CategoryName;
+    }
 }
