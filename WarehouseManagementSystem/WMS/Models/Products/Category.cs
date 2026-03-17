530a951b91b67a29cc2d095a75cc40eedf341c17
gnoohtwh <thongismyname1507@gmail.com>
2026-03-17 13:45:48 +0700

    abstraction-HuuThong

---
+++ WarehouseManagementSystem/WMS/Models/Products/Category.cs
@@ -0,0 +1,5 @@
+public class Category
+{
+    public string CategoryId { get; set; }
+    public string CategoryName { get; set; }
+}
---
+++ WarehouseManagementSystem/WMS/Models/Products/ElectronicProduct.cs
@@ -0,0 +1,7 @@
+using WarehouseManagementSystem.WinForms.Models.Products;
+
+public class ElectronicProduct : Product
+{
+    public string Specification { get; set; }
+    public int WarrantyPeriod { get; set; }
+}
