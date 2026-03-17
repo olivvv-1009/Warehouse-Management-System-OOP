0f49028278f9659021f73a53302656b42001f047
Lê Minh Anh <PC@LAPTOP-7A534JGV>
2026-03-17 14:12:24 +0700

    Edit depedency inventoryservice

---
+++ WarehouseManagementSystem/WMS/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WMS.Models.Inventory;
+using WMS.Models.Orders;
+using WMS.Models.Users;
+
+namespace WMS.Services
+{
+    public class OrderService
+    {
+        public void ProcessOrder(Order order, Warehouse warehouse)
+        {
+            order.Process(warehouse);
+        }
+          
+    }
+}
+
