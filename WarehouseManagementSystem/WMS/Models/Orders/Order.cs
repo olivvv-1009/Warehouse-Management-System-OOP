94aae539c701c90744bd18759e99169c3e97493f
Nhi Thao <nhinguyen.31241026988@st.ueh.edu.vn>
2026-03-17 14:40:14 +0700

    update import, export, order

--- WarehouseManagementSystem/WMS/Models/Orders/Order.cs
+++ WarehouseManagementSystem/WMS/Models/Orders/Order.cs
@@ -1,25 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using WMS.Models.Inventory;
 using WMS.Models.Users;
+
 namespace WMS.Models.Orders
 {
-    public class Order
+    // 👉 dùng abstract để thể hiện trừu tượng 
+    public abstract class Order
     {
+        public string OrderId { get; set; } = "";
+
+        // 👉 Aggregation/Composition với OrderDetail
         public List<OrderDetail> Details { get; set; }
+
+        // 👉 Association với Employee
         public Employee? Employee { get; set; }
+
         public Order()
         {
             Details = new List<OrderDetail>();
         }
+
         public Order(Employee employee)
         {
             Details = new List<OrderDetail>();
             Employee = employee;
         }
 
-        public virtual void Process(Warehouse warehouse)
+        public Order(string orderId, Employee employee)
         {
-            Console.WriteLine($"Order processed by: {Employee?.FullName}");
+            OrderId = orderId;
+            Employee = employee;
+            Details = new List<OrderDetail>();
         }
+
+        // 👉 method để thể hiện rõ association
+        public void AssignEmployee(Employee employee)
+        {
+            Employee = employee;
+        }
+
+        // 👉 abstract → bắt subclass phải override (đa hình + trừu tượng)
+        public abstract void Process(Warehouse warehouse);
     }
 }
