939f1288738ca919ab3e60b636852a35b0159b44
Thu <thudinh.31241026994@st.ueh.edu.vn>
2026-05-20 00:25:34 +0700

    return order

---
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/Repositories/ReturnRepository.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using WarehouseManagementSystem.WinForms.Files;
+using WarehouseManagementSystem.WinForms.Models;
+
+namespace WarehouseManagementSystem.WinForms.Repositories
+{
+    public class ReturnRepository
+    {
+        private const string FilePath =
+            "returnOrders.json";
+
+        private List<ReturnOrder> _returnOrders;
+
+        public ReturnRepository()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            _returnOrders =
+                FileHelper.ReadJsonList<ReturnOrder>(
+                    FilePath
+                );
+
+            if (_returnOrders == null)
+            {
+                _returnOrders =
+                    new List<ReturnOrder>();
+            }
+        }
+
+        private void SaveData()
+        {
+            FileHelper.WriteJsonList(
+                FilePath,
+                _returnOrders
+            );
+        }
+
+        public List<ReturnOrder> GetAll()
+        {
+            return _returnOrders;
+        }
+
+        public bool AddReturnOrder(
+            ReturnOrder returnOrder)
+        {
+            _returnOrders.Add(returnOrder);
+
+            SaveData();
+
+            return true;
+        }
+
+        public bool AddDetail(
+            string returnOrderId,
+            ReturnOrderDetail detail)
+        {
+            for (int i = 0;
+                 i < _returnOrders.Count;
+                 i++)
+            {
+                if (_returnOrders[i]
+                    .ReturnOrderId ==
+                    returnOrderId)
+                {
+                    _returnOrders[i]
+                        .Details
+                        .Add(detail);
+
+                    SaveData();
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Update(
+            List<ReturnOrder> returnOrders)
+        {
+            _returnOrders = returnOrders;
+
+            SaveData();
+        }
+
+        public void Delete(
+            string returnOrderId)
+        {
+            for (int i = 0;
+                 i < _returnOrders.Count;
+                 i++)
+            {
+                if (_returnOrders[i]
+                    .ReturnOrderId ==
+                    returnOrderId)
+                {
+                    _returnOrders
+                        .RemoveAt(i);
+
+                    SaveData();
+
+                    break;
+                }
+            }
+        }
+
+        public ReturnOrder FindById(
+            string returnOrderId)
+        {
+            for (int i = 0;
+                 i < _returnOrders.Count;
+                 i++)
+            {
+                if (_returnOrders[i]
+                    .ReturnOrderId ==
+                    returnOrderId)
+                {
+                    return _returnOrders[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/Services/ReturnService.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using WarehouseManagementSystem.WinForms.Models;
+using WarehouseManagementSystem.WinForms.Repositories;
+
+namespace WarehouseManagementSystem.WinForms.Services
+{
+    public class ReturnService
+    {
+        private ReturnRepository _repository;
+
+        public ReturnService()
+        {
+            _repository =
+                new ReturnRepository();
+        }
+
+        public List<ReturnOrder> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public bool CreateReturnOrder(
+            ReturnOrder returnOrder)
+        {
+            if (returnOrder == null)
+            {
+                return false;
+            }
+
+            if (returnOrder.ReturnOrderId == "")
+            {
+                return false;
+            }
+
+            return _repository
+                .AddReturnOrder(returnOrder);
+        }
+
+        public bool AddProductToReturnOrder(
+            string returnOrderId,
+            ReturnOrderDetail detail)
+        {
+            if (detail == null)
+            {
+                return false;
+            }
+
+            if (detail.ProductId == "")
+            {
+                return false;
+            }
+
+            if (detail.Quantity <= 0)
+            {
+                return false;
+            }
+
+            return _repository.AddDetail(
+                returnOrderId,
+                detail
+            );
+        }
+
+        public ReturnOrder FindById(
+            string returnOrderId)
+        {
+            return _repository.FindById(
+                returnOrderId
+            );
+        }
+
+        public void Delete(
+            string returnOrderId)
+        {
+            _repository.Delete(
+                returnOrderId
+            );
+        }
+
+        public void Update(
+            List<ReturnOrder> returnOrders)
+        {
+            _repository.Update(
+                returnOrders
+            );
+        }
+    }
+}
