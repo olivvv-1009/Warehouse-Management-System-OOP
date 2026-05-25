5c77aacec9a2c4ecd82f4a05d088512b5c999e44
Minh Anh <anhminhle26022006@gmail.com>
2026-05-25 15:17:17 +0700

    fix UI

--- WarehouseManagementSystem/WarehouseManagementSystem.WinForms/UI/Forms/Employees/EmployeeForm.Designer.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/UI/Forms/Employees/EmployeeForm.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
-            panel1 = new Panel();
-            btnAdd = new Button();
-            label1 = new Label();
             panel2 = new Panel();
             txtSearch = new TextBox();
             panel3 = new Panel();
@@ -47,20 +44,27 @@
             Edit = new DataGridViewButtonColumn();
             Delete = new DataGridViewButtonColumn();
             Reset = new DataGridViewButtonColumn();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            panel1 = new Panel();
+            label1 = new Label();
+            panel4 = new Panel();
+            btnAdd = new Button();
             tableLayoutPanel1.SuspendLayout();
-            panel1.SuspendLayout();
             panel2.SuspendLayout();
             panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvEmployee).BeginInit();
+            tableLayoutPanel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
             tableLayoutPanel1.Controls.Add(panel2, 0, 1);
             tableLayoutPanel1.Controls.Add(panel3, 0, 2);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -71,41 +75,9 @@
             tableLayoutPanel1.Size = new Size(1262, 693);
             tableLayoutPanel1.TabIndex = 0;
             // 
-            // panel1
-            // 
-            panel1.Controls.Add(btnAdd);
-            panel1.Controls.Add(label1);
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(3, 3);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1256, 90);
-            panel1.TabIndex = 0;
-            // 
-            // btnAdd
-            // 
-            btnAdd.BackColor = Color.SteelBlue;
-            btnAdd.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnAdd.ForeColor = SystemColors.ButtonHighlight;
-            btnAdd.Location = new Point(1019, 20);
-            btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(240, 45);
-            btnAdd.TabIndex = 1;
-            btnAdd.Text = "+ Add Employee";
-            btnAdd.UseVisualStyleBackColor = false;
-            btnAdd.Click += btnAdd_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(18, 21);
-            label1.Name = "label1";
-            label1.Size = new Size(173, 38);
-            label1.TabIndex = 0;
-            label1.Text = "Employees";
-            // 
             // panel2
             // 
+            panel2.BackColor = Color.White;
             panel2.Controls.Add(txtSearch);
             panel2.Dock = DockStyle.Fill;
             panel2.Location = new Point(3, 99);
@@ -116,9 +88,9 @@
             // txtSearch
             // 
             txtSearch.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtSearch.Location = new Point(18, 29);
+            txtSearch.Location = new Point(39, 29);
             txtSearch.Name = "txtSearch";
-            txtSearch.Size = new Size(1235, 36);
+            txtSearch.Size = new Size(1220, 36);
             txtSearch.TabIndex = 0;
             txtSearch.TextChanged += txtSearch_TextChanged;
             txtSearch.Enter += txtSearch_Enter;
@@ -138,7 +110,7 @@
             // 
             dgvEmployee.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvEmployee.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            dgvEmployee.BackgroundColor = Color.White;
+            dgvEmployee.BackgroundColor = SystemColors.Control;
             dgvEmployee.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvEmployee.Columns.AddRange(new DataGridViewColumn[] { EmployeeId, FullName, DateOfBirth, Gender, PhoneNumber, Email, Address, Role, Edit, Delete, Reset });
             dgvEmployee.Dock = DockStyle.Fill;
@@ -221,6 +193,63 @@
             Reset.Text = "Reset";
             Reset.UseColumnTextForButtonValue = true;
             // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel2.Controls.Add(panel4, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 3);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(1256, 90);
+            tableLayoutPanel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(622, 84);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(18, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(173, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Employees";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(btnAdd);
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(631, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(622, 84);
+            panel4.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.SteelBlue;
+            btnAdd.Font = new Font("Times New Roman", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdd.ForeColor = SystemColors.ButtonHighlight;
+            btnAdd.Location = new Point(370, 15);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(240, 55);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "+ Add Employee";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
             // EmployeeForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -230,12 +259,14 @@
             Size = new Size(1262, 693);
             Load += EmployeeForm_Load;
             tableLayoutPanel1.ResumeLayout(false);
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dgvEmployee).EndInit();
+            tableLayoutPanel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -260,5 +291,7 @@
         private DataGridViewButtonColumn Edit;
         private DataGridViewButtonColumn Delete;
         private DataGridViewButtonColumn Reset;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Panel panel4;
     }
 }
--- WarehouseManagementSystem/WarehouseManagementSystem.WinForms/UI/Forms/Suppliers/SupplierForm.Designer.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/UI/Forms/Suppliers/SupplierForm.Designer.cs
@@ -30,9 +30,6 @@
         {
             components = new System.ComponentModel.Container();
             tableLayoutPanel1 = new TableLayoutPanel();
-            panel1 = new Panel();
-            btnAdd = new Button();
-            label1 = new Label();
             panel2 = new Panel();
             txtSearch = new TextBox();
             panel3 = new Panel();
@@ -44,12 +41,19 @@
             address = new DataGridViewTextBoxColumn();
             Edit = new DataGridViewButtonColumn();
             Delete = new DataGridViewButtonColumn();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            panel1 = new Panel();
+            label1 = new Label();
+            panel4 = new Panel();
+            btnAdd = new Button();
             contextMenuStrip1 = new ContextMenuStrip(components);
             tableLayoutPanel1.SuspendLayout();
-            panel1.SuspendLayout();
             panel2.SuspendLayout();
             panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvSupplier).BeginInit();
+            tableLayoutPanel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -57,9 +61,9 @@
             tableLayoutPanel1.BackColor = SystemColors.Control;
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
             tableLayoutPanel1.Controls.Add(panel2, 0, 1);
             tableLayoutPanel1.Controls.Add(panel3, 0, 2);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -69,40 +73,6 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 468F));
             tableLayoutPanel1.Size = new Size(1435, 688);
             tableLayoutPanel1.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = SystemColors.Control;
-            panel1.Controls.Add(btnAdd);
-            panel1.Controls.Add(label1);
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(3, 3);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1429, 94);
-            panel1.TabIndex = 0;
-            // 
-            // btnAdd
-            // 
-            btnAdd.BackColor = Color.CornflowerBlue;
-            btnAdd.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnAdd.ForeColor = Color.White;
-            btnAdd.Location = new Point(1210, 23);
-            btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(216, 53);
-            btnAdd.TabIndex = 1;
-            btnAdd.Text = "+ Add Supplier";
-            btnAdd.UseVisualStyleBackColor = false;
-            btnAdd.Click += btnAdd_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(34, 38);
-            label1.Name = "label1";
-            label1.Size = new Size(154, 38);
-            label1.TabIndex = 0;
-            label1.Text = "Suppliers";
             // 
             // panel2
             // 
@@ -197,6 +167,64 @@
             Delete.Text = "Delete";
             Delete.UseColumnTextForButtonValue = true;
             // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel2.Controls.Add(panel4, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 3);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(1429, 94);
+            tableLayoutPanel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(708, 88);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(18, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Suppliers";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(btnAdd);
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(717, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(709, 88);
+            panel4.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.SteelBlue;
+            btnAdd.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdd.ForeColor = Color.White;
+            btnAdd.Location = new Point(402, 18);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(216, 53);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "+ Add Supplier";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
             // contextMenuStrip1
             // 
             contextMenuStrip1.ImageScalingSize = new Size(20, 20);
@@ -213,12 +241,14 @@
             Size = new Size(1435, 688);
             Load += SupplierForm_Load;
             tableLayoutPanel1.ResumeLayout(false);
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dgvSupplier).EndInit();
+            tableLayoutPanel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -240,5 +270,7 @@
         private DataGridViewTextBoxColumn address;
         private DataGridViewButtonColumn Edit;
         private DataGridViewButtonColumn Delete;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Panel panel4;
     }
 }
