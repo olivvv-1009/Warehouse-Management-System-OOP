6917e2a8492e6eb5c998a48c0b761f0136618630
Minh Anh <anhminhle26022006@gmail.com>
2026-05-27 18:30:50 +0700

    fix errors

--- WarehouseManagementSystem/WarehouseManagementSystem.WinForms/UI/Forms/Export/CreateExportInvoice.Designer.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem.WinForms/UI/Forms/Export/CreateExportInvoice.Designer.cs
@@ -71,9 +71,10 @@
             panelMain.Controls.Add(label1);
             panelMain.Dock = DockStyle.Fill;
             panelMain.Location = new Point(0, 0);
+            panelMain.Margin = new Padding(2, 2, 2, 2);
             panelMain.Name = "panelMain";
-            panelMain.Padding = new Padding(25);
-            panelMain.Size = new Size(678, 678);
+            panelMain.Padding = new Padding(20, 20, 20, 20);
+            panelMain.Size = new Size(1044, 542);
             panelMain.TabIndex = 0;
             // 
             // tableButton
@@ -85,41 +86,45 @@
             tableButton.Controls.Add(btnComplete, 2, 0);
             tableButton.Controls.Add(btnSaveDraft, 1, 0);
             tableButton.Controls.Add(btnCancel, 0, 0);
-            tableButton.Location = new Point(303, 596);
+            tableButton.Location = new Point(650, 473);
+            tableButton.Margin = new Padding(2, 2, 2, 2);
             tableButton.Name = "tableButton";
             tableButton.RowCount = 1;
             tableButton.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableButton.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableButton.Size = new Size(347, 54);
+            tableButton.RowStyles.Add(new RowStyle(SizeType.Absolute, 43F));
+            tableButton.Size = new Size(372, 47);
             tableButton.TabIndex = 5;
             // 
             // btnComplete
             // 
-            btnComplete.Anchor = AnchorStyles.None;
             btnComplete.BackColor = Color.LightCoral;
-            btnComplete.Location = new Point(233, 10);
+            btnComplete.Dock = DockStyle.Fill;
+            btnComplete.Location = new Point(246, 2);
+            btnComplete.Margin = new Padding(2, 2, 2, 2);
             btnComplete.Name = "btnComplete";
-            btnComplete.Size = new Size(108, 34);
+            btnComplete.Size = new Size(124, 43);
             btnComplete.TabIndex = 2;
             btnComplete.Text = "Complete";
             btnComplete.UseVisualStyleBackColor = false;
             // 
             // btnSaveDraft
             // 
-            btnSaveDraft.Anchor = AnchorStyles.None;
-            btnSaveDraft.Location = new Point(117, 10);
+            btnSaveDraft.Dock = DockStyle.Fill;
+            btnSaveDraft.Location = new Point(124, 2);
+            btnSaveDraft.Margin = new Padding(2, 2, 2, 2);
             btnSaveDraft.Name = "btnSaveDraft";
-            btnSaveDraft.Size = new Size(108, 34);
+            btnSaveDraft.Size = new Size(118, 43);
             btnSaveDraft.TabIndex = 1;
             btnSaveDraft.Text = "Save Draft";
             btnSaveDraft.UseVisualStyleBackColor = true;
             // 
             // btnCancel
             // 
-            btnCancel.Anchor = AnchorStyles.None;
-            btnCancel.Location = new Point(3, 10);
+            btnCancel.Dock = DockStyle.Fill;
+            btnCancel.Location = new Point(2, 2);
+            btnCancel.Margin = new Padding(2, 2, 2, 2);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(108, 34);
+            btnCancel.Size = new Size(118, 43);
             btnCancel.TabIndex = 0;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = true;
@@ -129,18 +134,20 @@
             panel1.BackColor = Color.AliceBlue;
             panel1.Controls.Add(label7);
             panel1.Controls.Add(label6);
-            panel1.Location = new Point(26, 476);
+            panel1.Location = new Point(21, 381);
+            panel1.Margin = new Padding(2, 2, 2, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(624, 76);
+            panel1.Size = new Size(1001, 61);
             panel1.TabIndex = 4;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.ForeColor = SystemColors.HotTrack;
-            label7.Location = new Point(18, 38);
+            label7.Location = new Point(14, 30);
+            label7.Margin = new Padding(2, 0, 2, 0);
             label7.Name = "label7";
-            label7.Size = new Size(556, 25);
+            label7.Size = new Size(467, 20);
             label7.TabIndex = 1;
             label7.Text = "Products will be automatically allocated from the oldest batches first.";
             // 
@@ -150,21 +157,24 @@
             label6.BackColor = Color.AliceBlue;
             label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label6.ForeColor = Color.RoyalBlue;
-            label6.Location = new Point(18, 13);
+            label6.Location = new Point(14, 10);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(219, 25);
+            label6.Size = new Size(183, 20);
             label6.TabIndex = 0;
             label6.Text = "FIFO (First-In-First-Out): ";
             // 
             // dgvProducts
             // 
+            dgvProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvProducts.Columns.AddRange(new DataGridViewColumn[] { Product, Available, Quantity, FIFO_Allocation });
-            dgvProducts.Location = new Point(26, 316);
+            dgvProducts.Location = new Point(21, 253);
+            dgvProducts.Margin = new Padding(2, 2, 2, 2);
             dgvProducts.Name = "dgvProducts";
             dgvProducts.RowHeadersVisible = false;
             dgvProducts.RowHeadersWidth = 62;
-            dgvProducts.Size = new Size(624, 122);
+            dgvProducts.Size = new Size(1001, 98);
             dgvProducts.TabIndex = 3;
             // 
             // Product
@@ -172,28 +182,24 @@
             Product.HeaderText = "Product";
             Product.MinimumWidth = 8;
             Product.Name = "Product";
-            Product.Width = 170;
             // 
             // Available
             // 
             Available.HeaderText = "Available";
             Available.MinimumWidth = 8;
             Available.Name = "Available";
-            Available.Width = 120;
             // 
             // Quantity
             // 
             Quantity.HeaderText = "Quantity";
             Quantity.MinimumWidth = 8;
             Quantity.Name = "Quantity";
-            Quantity.Width = 120;
             // 
             // FIFO_Allocation
             // 
             FIFO_Allocation.HeaderText = "FIFO_Allocation";
             FIFO_Allocation.MinimumWidth = 8;
             FIFO_Allocation.Name = "FIFO_Allocation";
-            FIFO_Allocation.Width = 220;
             // 
             // tableProductHeader
             // 
@@ -202,11 +208,12 @@
             tableProductHeader.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
             tableProductHeader.Controls.Add(label5, 0, 0);
             tableProductHeader.Controls.Add(btnAddProduct, 1, 0);
-            tableProductHeader.Location = new Point(26, 220);
+            tableProductHeader.Location = new Point(21, 176);
+            tableProductHeader.Margin = new Padding(2, 2, 2, 2);
             tableProductHeader.Name = "tableProductHeader";
             tableProductHeader.RowCount = 1;
             tableProductHeader.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableProductHeader.Size = new Size(624, 60);
+            tableProductHeader.Size = new Size(1001, 48);
             tableProductHeader.TabIndex = 2;
             // 
             // label5
@@ -214,9 +221,10 @@
             label5.Anchor = AnchorStyles.Left;
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label5.Location = new Point(3, 17);
+            label5.Location = new Point(2, 14);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(100, 25);
+            label5.Size = new Size(82, 20);
             label5.TabIndex = 0;
             label5.Text = "Products *";
             // 
@@ -225,9 +233,10 @@
             btnAddProduct.Anchor = AnchorStyles.None;
             btnAddProduct.BackColor = Color.SeaShell;
             btnAddProduct.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            btnAddProduct.Location = new Point(447, 13);
+            btnAddProduct.Location = new Point(784, 10);
+            btnAddProduct.Margin = new Padding(2, 2, 2, 2);
             btnAddProduct.Name = "btnAddProduct";
-            btnAddProduct.Size = new Size(165, 34);
+            btnAddProduct.Size = new Size(132, 27);
             btnAddProduct.TabIndex = 1;
             btnAddProduct.Text = "+ Add Product";
             btnAddProduct.UseVisualStyleBackColor = false;
@@ -244,13 +253,14 @@
             tableLayoutPanel1.Controls.Add(lblEmployeeValue, 2, 1);
             tableLayoutPanel1.Controls.Add(txtDestination, 0, 1);
             tableLayoutPanel1.Controls.Add(dtpDate, 1, 1);
-            tableLayoutPanel1.Location = new Point(28, 79);
+            tableLayoutPanel1.Location = new Point(22, 63);
+            tableLayoutPanel1.Margin = new Padding(2, 2, 2, 2);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(622, 104);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 16F));
+            tableLayoutPanel1.Size = new Size(1000, 83);
             tableLayoutPanel1.TabIndex = 1;
             tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
             // 
@@ -259,9 +269,10 @@
             label4.Anchor = AnchorStyles.None;
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label4.Location = new Point(419, 13);
+            label4.Location = new Point(751, 10);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(193, 25);
+            label4.Size = new Size(157, 20);
             label4.TabIndex = 2;
             label4.Text = "Created By Employee";
             // 
@@ -270,9 +281,10 @@
             label3.Anchor = AnchorStyles.None;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label3.Location = new Point(275, 13);
+            label3.Location = new Point(468, 10);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(65, 25);
+            label3.Size = new Size(53, 20);
             label3.TabIndex = 1;
             label3.Text = "Date *";
             label3.Click += label3_Click;
@@ -282,9 +294,10 @@
             label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label2.Location = new Point(41, 13);
+            label2.Location = new Point(114, 10);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(123, 25);
+            label2.Size = new Size(101, 20);
             label2.TabIndex = 0;
             label2.Text = "Destination *";
             // 
@@ -292,46 +305,51 @@
             // 
             lblEmployeeValue.Anchor = AnchorStyles.None;
             lblEmployeeValue.AutoSize = true;
-            lblEmployeeValue.Location = new Point(516, 65);
+            lblEmployeeValue.Location = new Point(830, 52);
+            lblEmployeeValue.Margin = new Padding(2, 0, 2, 0);
             lblEmployeeValue.Name = "lblEmployeeValue";
-            lblEmployeeValue.Size = new Size(0, 25);
+            lblEmployeeValue.Size = new Size(0, 20);
             lblEmployeeValue.TabIndex = 5;
             // 
             // txtDestination
             // 
             txtDestination.Anchor = AnchorStyles.None;
-            txtDestination.Location = new Point(27, 62);
+            txtDestination.Location = new Point(10, 48);
+            txtDestination.Margin = new Padding(2, 2, 2, 2);
             txtDestination.Name = "txtDestination";
-            txtDestination.Size = new Size(150, 31);
+            txtDestination.Size = new Size(310, 27);
             txtDestination.TabIndex = 3;
             // 
             // dtpDate
             // 
-            dtpDate.Anchor = AnchorStyles.Bottom;
+            dtpDate.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             dtpDate.CustomFormat = "MM/dd/yyyy";
             dtpDate.DropDownAlign = LeftRightAlignment.Right;
             dtpDate.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            dtpDate.Location = new Point(225, 70);
+            dtpDate.Location = new Point(332, 48);
+            dtpDate.Margin = new Padding(2, 2, 2, 2);
             dtpDate.Name = "dtpDate";
-            dtpDate.Size = new Size(164, 31);
+            dtpDate.Size = new Size(326, 27);
             dtpDate.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-            label1.Location = new Point(26, 9);
+            label1.Location = new Point(21, 7);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(342, 45);
+            label1.Size = new Size(294, 37);
             label1.TabIndex = 0;
             label1.Text = "Create Export Invoice";
             // 
             // CreateExportInvoice
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(678, 678);
+            ClientSize = new Size(1044, 542);
             Controls.Add(panelMain);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "CreateExportInvoice";
             Text = "CreateExportInvoice";
             panelMain.ResumeLayout(false);
